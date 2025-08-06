0d4813141d9dcd1e7d4137d5a1bdf06906d66108
Fandatra <efandareyosaputra01@gmail.com>
2025-08-06 16:29:11 +0700

    feat

--- app/src/main/java/com/smkth/app3_recycleview/utils/DummyData.kt
+++ app/src/main/java/com/smkth/app3_recycleview/utils/DummyData.kt
@@ -5,9 +5,16 @@ import com.smkth.app3_recycleview.model.Student
 object DummyData{
     fun getStudentList(): List<Student> {
         return listOf(
-            Student("Andi", "123456", "XII TJKT 1"),
-            Student("Okta", "123456", "XII TJKT 1"),
-            Student("Soli", "123456", "XII TJKT 1")
+            Student("Andi", "1001", "XII TJKT 1"),
+            Student("Budi", "1002", "XII TJKT 1"),
+            Student("Citra", "1003", "XII TJKT 1"),
+            Student("Doni", "1004", "XII TJKT 1"),
+            Student("Eka", "1005", "XII TJKT 1"),
+            Student("Fani", "1006", "XII TJKT 1"),
+            Student("Gilang", "1007", "XII TJKT 1"),
+            Student("Hani", "1008", "XII TJKT 1"),
+            Student("Iqbal", "1009", "XII TJKT 1"),
+            Student("Joko", "1010", "XII TJKT 1")
         )
     }
 }
